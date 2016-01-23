6814cc2a9ec057bbaea25adb5f1f1f2cae2bc93e
Andrew Hryshmanovkii <grishman4life@gmail.com>
2016-01-23 19:21:33 +0200

    Update widget with shape drawables

--- app/src/main/java/circularinformer/grishman/com/CircleInformerWidget.java
+++ app/src/main/java/circularinformer/grishman/com/CircleInformerWidget.java
@@ -1,7 +1,31 @@
 package circularinformer.grishman.com;
 
+import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
 
+/**
+ * Widget to inform user about network state
+ */
 public class CircleInformerWidget extends AppWidgetProvider {
 
+    public CircleInformerWidget() {
+        super();
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        super.onEnabled(context);
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
 }
